88c2cd5520b9b430872462eb90b2ee8ecaa41d04
Usman Liaqat <7531531+usmanbinliaqat@users.noreply.github.com>
2018-09-05 16:58:29 +0500

    Code re-structure

--- GeolocationParams.ts
+++ GeolocationParams.ts
@@ -1,40 +1,39 @@
 export class GeolocationParams  {
+	ip: string;
+	fields: string;
+	ips: any;
+ 
+	constructor() {
+		this.ip = "";
+		this.fields = "";
+		this.ips = "";
+	}
 
-      ip: string;
-      fields: string;
-      ips: any;
-      
-    constructor() {
-	 this.ip = "";
-	 this.fields = "";
-     this.ips = "";
-    }
+	setIp(ip = "") {
+		this.ip = ip;
+	}
 
-    setIp(ip = "") {
-      this.ip = ip;
-    }
+	getIp() {
+		return this.ip;
+	}
 
-    getIp() {
-        return this.ip;
-    }
+	setFields(fields = "") {
+		this.fields = fields;
+	}
 
-    setFields(fields = "") {
-        this.fields = fields;
-    }
+	getFields() {
+		return this.fields;
+	}
 
-    getFields() {
-        return this.fields;
-    }
+	setIps(ips : any = null) {
+		if(ips.length > 50) {
+			console.log("Max. number of IP addresses cannot be more than 50.");
+		} else {
+			this.ips = ips;
+		}
+	}
 
-    setIps(ips : any = null) {
-        if(ips.length > 50) {
-            console.log("Max. number of IP addresses cannot be more than 50.");
-        } else {
-            this.ips = ips;
-        }
-    }
-
-    getIps() {
-        return this.ips;
-    }
+	getIps() {
+		return this.ips;
+	}
 }
