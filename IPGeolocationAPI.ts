d9625be6cc96ca8bf02cb9e28a50c243d33441c8
Usman Liaqat <7531531+usmanbinliaqat@users.noreply.github.com>
2018-09-05 16:55:15 +0500

    Code re-structure

--- IPGeolocationAPI.ts
+++ IPGeolocationAPI.ts
@@ -2,7 +2,6 @@ import {GeolocationParams} from './GeolocationParams';
 import {TimezoneParams} from './TimezoneParams';
 import {XMLHttpRequest} from './node_modules/xmlhttprequest'
 export class IPGeolocationAPI {
-
     apiKey: string;
 
     constructor(apiKey: string) {
@@ -14,44 +13,44 @@ export class IPGeolocationAPI {
     }
 
     public getGeolocation(params : GeolocationParams = null) {
-        if(params.getIps())
-        { return this.postRequest("ipgeo-bulk", params, this.apiKey);
-         }else{
-          return this.getRequest("ipgeo", this.buildGeolocationUrlParams(params, this.apiKey));
+        if(params.getIps()) {
+            return this.postRequest("ipgeo-bulk", params, this.apiKey);
+         } else {
+             return this.getRequest("ipgeo", this.buildGeolocationUrlParams(params, this.apiKey));
          }
     }
      
     public getTimezone(params : TimezoneParams = null) {
         return this.getRequest("timezone", this.buildTimezoneUrlParams(params, this.apiKey));
     }
-
     
     private buildTimezoneUrlParams(params=null, apiKey="") {
-    var urlParams = "apiKey=" + apiKey;
+        var urlParams = "apiKey=" + apiKey;
 
-    if(params != null) {
-        var param = params.getIp();
-        if(param && param != "") {
-            urlParams = urlParams + "&ip=" + param;
-        }
+        if(params != null) {
+            var param = params.getIp();
 
-        param = params.getTimezone();
-        if(param && param != "") {
-            urlParams = urlParams + "&tz=" + param;
-        }
+            if(param && param != "") {
+                urlParams = urlParams + "&ip=" + param;
+            }
 
-        var latitude = params.getLatitude();
-        var longitude = params.getLongitude();
-        if(latitude && latitude != 1000.0 && longitude && longitude != 1000.0) {
-            urlParams = urlParams + "&lat=" + latitude + "&long=" + longitude;
+            param = params.getTimezone();
+            if(param && param != "") {
+                urlParams = urlParams + "&tz=" + param;
+            }
+
+            var latitude = params.getLatitude();
+            var longitude = params.getLongitude();
+            if(latitude && latitude != 1000.0 && longitude && longitude != 1000.0) {
+                urlParams = urlParams + "&lat=" + latitude + "&long=" + longitude;
+            }
         }
-    }
-    return urlParams;
+        return urlParams;
     }
 
     private buildGeolocationUrlParams(params=null, apiKey="") {
-        
         var urlParams = "apiKey=" + apiKey;
+        
         if(params != null) {
             var param = params.getIp();
            
@@ -60,80 +59,63 @@ export class IPGeolocationAPI {
             }
 
             param = params.getFields();
-        
             if(param && param != "") {
                 urlParams = urlParams + "&fields=" + param;
             }
         }
-
         return urlParams;
     }
     
 
     private getRequest(subUrl = "", params = ""){
-
-    var jsonData = null;
-    var data = null;
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-    xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === 4) {
-            if(this.status == 0){
-                jsonData = {
-                "message": "Internet is not connected!"
-                };
-            }else{
-                jsonData = JSON.parse(this.responseText);
+        var jsonData = null;
+        var data = null;
+        var xhr = new XMLHttpRequest();
+        
+        xhr.withCredentials = true;
+        xhr.addEventListener("readystatechange", function () {
+            if(this.readyState === 4) {
+                if(this.status == 0) {
+                    jsonData = {
+                        "message": "Internet is not connected!"
+                    };
+                } else {
+                    jsonData = JSON.parse(this.responseText);
+                }
             }
-    }
-    });
-    console.log("https://api.ipgeolocation.io/"+subUrl+"?"+params+"");
-    xhr.open("GET", "https://api.ipgeolocation.io/"+subUrl+"?"+params+"", false);
-    xhr.send(data);
-    return jsonData;
-    
+        });
+        
+        // console.log("https://api.ipgeolocation.io/" + subUrl + "?" + params + "");
+        xhr.open("GET", "https://api.ipgeolocation.io/" + subUrl + "?" + params + "", false);
+        xhr.send(data);
+        
+        return jsonData;
     }
 
-    private postRequest(subUrl = "", params : GeolocationParams = null, apiKey=""){
-
+    private postRequest(subUrl="", params: GeolocationParams=null, apiKey="") {
         var jsonData = null; 
         var data = JSON.stringify({
           "ips": params.getIps()
         });
         var xhr = new XMLHttpRequest();
+        
         xhr.withCredentials = true;
         xhr.addEventListener("readystatechange", function () {
             if (this.readyState === 4) {
-                if(this.status == 0){
+                if(this.status == 0) {
                     jsonData = {
-                    "message": "Internet is not connected!"
+                        "message": "Internet is not connected!"
                     };
-                }else{
-                      jsonData = JSON.parse(this.responseText);
+                } else {
+                    jsonData = JSON.parse(this.responseText);
                 }
             }
         });
-        xhr.open("POST", "https://api.ipgeolocation.io/"+subUrl+"?apiKey="+apiKey+"", false);
+        
+        xhr.open("POST", "https://api.ipgeolocation.io/" + subUrl + "?apiKey=" + apiKey + "", false);
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send(data);
 
         return jsonData;
-        
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
